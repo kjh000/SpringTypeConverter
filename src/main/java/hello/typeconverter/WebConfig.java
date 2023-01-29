11bb036b3776c3f3adc6759cc99cd8b0ae173882
origin <wer2wer306@gmail.com>
2023-01-29 15:32:38 +0900

    스프링 타입 컨버터

---
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,32 @@
+package hello.typeconverter;
+
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 추가하고 싶은 컨버터 등록
+     * 스프링은 기본타입에 대한 컨버터는 내부적으로 제공함
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+        // 우선순위 때문에 주석처리함
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
+    }
+}
